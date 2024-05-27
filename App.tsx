3ebd5674728d28f89d9a23145f9f865b6424d0e3
Amber Jain <jainanuamber@gmail.com>
2024-05-27 23:51:06 +0530

    updated components

--- App.tsx
+++ App.tsx
@@ -2,10 +2,32 @@ import { SafeAreaView, ScrollView } from "react-native";
 import SearchBox from "./components/SearchBox";
 
 export default function App() {
+  const search: Function = (searchTerm: string) => {
+    if (searchTerm == "") {
+      alert("Please enter a search term");
+      return;
+    }
+    const fetchApiRequest = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${searchTerm}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+        throw error;
+      }
+    };
+    fetchApiRequest().then((response) => console.log(response));
+  };
   return (
     <SafeAreaView className='flex-1 bg-black'>
       <ScrollView keyboardShouldPersistTaps='handled' className='h-screen'>
-        <SearchBox />
+        <SearchBox onSearch={search} />
       </ScrollView>
     </SafeAreaView>
   );
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,7 +1,9 @@
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 
-type Props = {};
+type Props = {
+  onSearch: Function;
+};
 
 const SearchBox = (props: Props) => {
   const [userName, setUserName] = useState<String>("");
@@ -10,10 +12,12 @@ const SearchBox = (props: Props) => {
       <TextInput
         placeholder='Enter Your Username Here'
         placeholderTextColor='#FFFFFF'
-        className='border border-gray-200 p-2'
+        className='border border-gray-200 p-2 w-2/3 text-gray-100'
         onChangeText={(text: String) => setUserName(text)}
       />
-      <TouchableOpacity className='bg-sky-600 px-4 py-2 rounded'>
+      <TouchableOpacity
+        className='bg-sky-600 px-4 py-2 rounded'
+        onPress={() => props.onSearch(userName)}>
         <Text className='text-gray-50'>Search</Text>
       </TouchableOpacity>
     </View>
